dd876d30b35132652dcd1c9a600b5779eaee67b5
Yuya Yoshida <yuu.yoshi12@outlook.jp>
2021-06-07 23:35:51 +0900

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(10);
+        Schema::defaultStringLength(191);
         URL::forceScheme('https');    //公開時は”https”に変更する。
 
         //全ページに対して現在年を渡す
