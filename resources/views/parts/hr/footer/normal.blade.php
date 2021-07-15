797c1cf89a8435db96dd586224a08bb04a42fb45
Yuu412 <yuu.yoshi12@outlook.jp>
2021-07-15 14:53:38 +0900

    create pp

--- resources/views/parts/hr/footer/normal.blade.php
+++ resources/views/parts/hr/footer/normal.blade.php
@@ -3,11 +3,11 @@
 		<img src="{{ asset('/img/logo/logo_origin.png') }}" alt="ロゴ">
 	</a>
 	<ul class="footer-nav">
-    <li><a href="">お問い合わせ</a></li>
-    <li><a href="" >プライバシーポリシー</a></li>
+    <li><a href="{{ route('hr.policy') }}">お問い合わせ</a></li>
+    <li><a href="https://www.pampam.co.jp/privacy/">プライバシーポリシー</a></li>
     <li><a href="https://www.pampam.co.jp/" >運営会社</a></li>
   </ul>
 	<div class="copyright">
-		<p>© 2021 - Organisation</p>
+		<p>Copyright © 2021 株式会社ぱむ Inc.All rights reserved</p>
 	</div>
 </footer>
