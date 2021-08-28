d610d7dc2b1e10317338bec99334478b95168ac7
Yuu412 <yuu.yoshi12@outlook.jp>
2021-08-28 20:35:51 +0900

    create HR page

--- resources/views/st/auth/already/register.blade.php
+++ resources/views/st/auth/already/register.blade.php
@@ -5,17 +5,16 @@
   <div class="block-wrapper">
     <div class="block-background"></div>
       <div class="block">
-        <h1>LINEアカウントと連携してください</h1>
+        <h1>LINEと連携してください</h1>
         <div class="img">
-          <img src="../img/unavailable/unavailable-contributor.svg" alt="面接官を探しているイラスト">
-          <a href="https://storyset.com/work" style="color: #EEE;font-size: 10px;">Work illustrations by Storyset</a>        
+          <img src="../img/unavailable/line-auth.svg" alt="LINEを連携しているイラスト">
         </div>
         <div class="description" style="font-size:14px">
-          面接図鑑ではLINE連携が必須となりました。<br>
+          面接図鑑ではLINE連携が<b>必須</b>となりました。<br>
           オファー通知などは公式LINEから行われます。<br>
-          「LINEと連携」よりLINEと連携してください。<br>
+          「LINE連携」よりLINEと連携してください。<br>
         </div>
-        @include('components.parts.button.line_button', ['text'=>'LINEと連携', 'routeName'=>'social_line.redirect', 'var'=>'line'])
+        @include('components.parts.button.line_button', ['text'=>'LINE連携', 'routeName'=>'social_line.redirect', 'var'=>'line'])
       </div>
   </div>
 @endsection
--- resources/views/st/unavailable/contributor.blade.php
+++ resources/views/st/unavailable/contributor.blade.php
@@ -1,19 +1,19 @@
-@section('title', 'まずは面接予約をしよう！')
+@section('title', 'まずは面接予約をしてください！')
 <link rel="stylesheet" href="{{ asset('css/st/unavailable/unavailable.css') }}">
 @extends('layouts.st.common')
 @section('content')
   <div class="block-wrapper">
     <div class="block-background"></div>
       <div class="block">
-        <h1>面接予約をしよう！</h1>
+        <h1>面接予約をしてください！</h1>
         <div class="img">
-          <img src="../img/unavailable/unavailable-contributor.svg" alt="面接官を探しているイラスト">
+          <img src="{{ asset('img/unavailable/unavailable-contributor.svg') }}" alt="面接官を探しているイラスト">
           <a href="https://storyset.com/work" style="color: #EEE;font-size: 10px;">Work illustrations by Storyset</a>        
         </div>
         <div class="description">
-          面接官を探しましょう！<br>
-          面接後、動画が公開されると、<br>
-          他の就活生の面接が見放題になります！<br>
+          現役人事と模擬面接を行ってください！<br>
+          模擬面接を行っていただくと、<br>
+          全国の就活生の面接をご覧いただけます！<br>
         </div>
         <a href="{{ route('interview.search') }}">
           <div class="button">
